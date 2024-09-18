4fa24e402b39747f288a4ffe77413040cc351f67
hashiharis <100787257+hashiharis@users.noreply.github.com>
2024-09-18 14:05:23 +0530

    added update status functionality

--- todo-app/src/components/todomodule/DisplayTodo.jsx
+++ todo-app/src/components/todomodule/DisplayTodo.jsx
@@ -1,101 +1,90 @@
-import './displaytodo.css'
-import { RiCheckboxBlankCircleLine } from "react-icons/ri";
-import { RiCheckboxCircleLine } from "react-icons/ri";
-import { TASK_STATUS, VISIBILITY_STATUS } from '../../constants/todoConstants';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { showAllItems, showCompletedItems, showPendingItems } from './todoSlice';
-export const DisplayTodo=()=>{
-    const [todoList,setToDoList]=useState([])
-    const dispatch=useDispatch()
-    const {items,currentVisiblityStatus}=useSelector(state=>state.todo)
-    const {SHOW_ALL,PENDING,COMPLETED}=VISIBILITY_STATUS;
-    const {TASK_PENDING,TASK_COMPLETED}=TASK_STATUS;
-    
-    useEffect(()=>{  
-     
-        if(currentVisiblityStatus===PENDING){
-          let pendingItems= items.filter((item)=>item.status===TASK_PENDING)
-        console.log(pendingItems)
-          setToDoList(pendingItems)
-          
-        }
-        else if(currentVisiblityStatus===COMPLETED){
-            let completedItems=items.filter((item)=>item.status===TASK_COMPLETED)
-            setToDoList(completedItems)
-            console.log(completedItems)
-        }
-        else{
-            setToDoList(items)
-        }
-        // console.log(items)
-    },[items,currentVisiblityStatus])
+import "./displaytodo.css";
+import { TASK_STATUS, VISIBILITY_STATUS } from "../../constants/todoConstants";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  handleStatusChange,
+  showAllItems,
+  showCompletedItems,
+  showPendingItems,
+} from "./todoSlice";
+export const DisplayTodo = () => {
+  const [todoList, setToDoList] = useState([]);
+  const dispatch = useDispatch();
+  const { items, currentVisibilityStatus } = useSelector((state) => state.todo);
+  const { SHOW_ALL, PENDING, COMPLETED } = VISIBILITY_STATUS;
+  const { TASK_PENDING, TASK_COMPLETED } = TASK_STATUS;
 
+  useEffect(() => {
+    console.log("Items:", items);
+    console.log("currentvisibilitystatus", currentVisibilityStatus);
 
-    const handleChange=(e)=>{
-       const {value}=e.target
-       if(value===COMPLETED){
-          dispatch(showCompletedItems())
-       }
-       else if(value===PENDING){
-        dispatch(showPendingItems())
-       }
-       else{
-        dispatch(showAllItems())
-       }
+    if (currentVisibilityStatus === PENDING) {
+      let pendingItems = items.filter((item) => item.status === TASK_PENDING);
+      console.log(pendingItems);
+      setToDoList(pendingItems);
+    } else if (currentVisibilityStatus === COMPLETED) {
+      let completedItems = items.filter(
+        (item) => item.status === TASK_COMPLETED
+      );
+      setToDoList(completedItems);
+      console.log(completedItems);
+    } else {
+      setToDoList(items);
     }
-    // const handleTaskStatusChange=()=>{
-        
-    // }
-    return(
-        <>
-        <div className='status-dropdown'>
-           <select onChange={handleChange}>
-            <option value={SHOW_ALL}>Show All</option>
-            <option value={PENDING}>Pending</option>
-            <option value={COMPLETED}>Completed</option>
-           </select>
-        </div>
-        {
-            todoList.map((item,index)=>{
-                return(
-                    <div key={index} className="task-container">
-                         <div className="task-list">
-                            {
-                                item.status===TASK_PENDING ? (
-                                    <>
-                                    <input type="checkbox"/>
-                                    <p>{item.name}</p>
-                                    </>
-                                    
-                                ):(
-                                    <>
-                                    <input type="checkbox"/>
-                                    <p>{item.name}</p>
-                                    </>
-                                    
-                                )
+    // console.log(items)
+  }, [items, currentVisibilityStatus]);
 
-                               
-                            }
-                         </div>
-                          {/* <div className="task-list">
-                                <input type="checkbox"
-                                /> */}
-                              {/* <span><RiCheckboxBlankCircleLine/></span> */}
-                              {/* <p>{item.name}</p>
-                          </div> */}
-                       
-                   
-                </div>
-                )
-            })
-        }
-       
-        </>
-    )
-}
+  const handleChange = (e) => {
+    const { value } = e.target;
+    if (value === COMPLETED) {
+      dispatch(showCompletedItems());
+    } else if (value === PENDING) {
+      dispatch(showPendingItems());
+    } else {
+      dispatch(showAllItems());
+    }
+  };
 
+  const handleTaskStatusChange = (checkedId) => {
+ 
+    console.log('Dispatching changes:',checkedId)
+    if(!checkedId){
+        return
+    }
+  dispatch(handleStatusChange(checkedId))
+  };
+
+  return (
+    <>
+      <div className="status-dropdown">
+        <select onChange={handleChange}>
+          <option value={SHOW_ALL}>Show All</option>
+          <option value={PENDING}>Pending</option>
+          <option value={COMPLETED}>Completed</option>
+        </select>
+      </div>
+      {todoList.map((item, index) => {
+        return (
+          <div key={index} className="task-container">
+            <div className="task-list">
+                <>
+                  <input
+                    type="checkbox"
+                    checked={item.status===TASK_COMPLETED?true:false}
+                    onChange={() => {
+                      handleTaskStatusChange(item.id);
+                    }}
+                  />
+                  <p>{item.name}</p>
+                </>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 // {item.status===TASK_PENDING?
 //     (
@@ -114,5 +103,5 @@ export const DisplayTodo=()=>{
 //           <p>{item.name}</p>
 //       </div>
 //     )
-         
+
 //     }
--- todo-app/src/components/todomodule/todoSlice.js
+++ todo-app/src/components/todomodule/todoSlice.js
@@ -1,46 +1,67 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { TASK_STATUS, TODO_SLICE_NAME, VISIBILITY_STATUS } from "../../constants/todoConstants";
+import {
+  TASK_STATUS,
+  TODO_SLICE_NAME,
+  VISIBILITY_STATUS,
+} from "../../constants/todoConstants";
 
+const {TASK_COMPLETED,TASK_PENDING}=TASK_STATUS;
+const {SHOW_ALL,PENDING,COMPLETED}=VISIBILITY_STATUS;
 const initialState = {
   items: [
     {
       id: Math.floor(Math.random() * 10000),
       name: "buy milk",
-      status: TASK_STATUS.TASK_PENDING,
+      status: TASK_PENDING,
     },
     {
       id: Math.floor(Math.random() * 10000),
       name: "go to gym",
-      status: TASK_STATUS.TASK_COMPLETED,
+      status: TASK_COMPLETED,
     },
   ],
-  currentVisibilityStatus:VISIBILITY_STATUS.SHOW_ALL,
+  currentVisibilityStatus: SHOW_ALL,
 };
 
 export const todoSlice = createSlice({
   name: TODO_SLICE_NAME,
   initialState,
   reducers: {
-    addTodoItem:(state,action)=>{
-      let obj={
+    addTodoItem: (state, action) => {
+      let obj = {
         id: Math.floor(Math.random() * 10000),
-        name:action.payload,
-        status:TASK_STATUS.TASK_PENDING,
+        name: action.payload,
+        status: TASK_PENDING,
+      };
+      state.items.push(obj);
+    },
+    showAllItems: (state) => {
+      state.currentVisibilityStatus = SHOW_ALL;
+    },
+    showPendingItems: (state) => {
+      state.currentVisibilityStatus = PENDING;
+    },
+    showCompletedItems: (state) => {
+      state.currentVisibilityStatus = COMPLETED;
+    },
+    handleStatusChange: (state, action) => {
+      const id=action.payload
+      console.log("Before updated Items:", state.items);
+      const item = state.items.find((item) => item.id === id);
+      if (item) {
+        item.status = item.status===TASK_COMPLETED?TASK_PENDING:TASK_COMPLETED
       }
-      state.items.push(obj)
+      console.log("After Updated Items:", state.items);
     },
-    showAllItems:(state)=>{
-        state.currentVisibilityStatus=VISIBILITY_STATUS.SHOW_ALL
-    },
-    showPendingItems:(state)=>{
-        state.currentVisibilityStatus=VISIBILITY_STATUS.PENDING
-    },
-    showCompletedItems:(state)=>{
-        state.currentVisibilityStatus=VISIBILITY_STATUS.COMPLETED
-    }
   },
 });
 
-export const {addTodoItem,showAllItems,showPendingItems,showCompletedItems}=todoSlice.actions
+export const {
+  addTodoItem,
+  showAllItems,
+  showPendingItems,
+  showCompletedItems,
+  handleStatusChange,
+} = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
